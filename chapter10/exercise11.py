6ae078b43f9a87246b2deed3dcb6f7159089c89c
abin paul <paulabin9@gmail.com>
2021-03-11 20:18:05 +0530

    modifies exercise11

--- chapter10/exercise11.py
+++ chapter10/exercise11.py
@@ -1,23 +1,37 @@
-"""to check whether the word is reverse order"""
-def reverse_pair(t):
-    count = 0
-    count2 = -1
-    if len(t) <= 2:
+def reverse_pair(word):
+    a = word[::-1]
+    return a
+
+def reverse_pair_list(word):
+    reverse_list = []
+    for i in word:
+        reverse_list.append(reverse_pair(i.strip()))
+    return reverse_list
+
+def normal_list(word):
+    lists = []
+    for i in word:
+        lists.append(i.strip())
+    return lists
+
+def in_bisecet(pac_list, a):
+    mid = int(len(pac_list) / 2)
+    if mid == 0:
+        return False
+    if pac_list[mid] == a:
         return True
-    for i in range(len(t)):
-        if t[count] != t[count2]:
-            return False
-        count += 1
-        count2 += -1
-    return True        
+    elif pac_list[mid] < a:
+        b = pac_list[mid:len(pac_list)]
+        return in_bisecet(b,a)
+    else:
+        c = pac_list[:mid]
+        return in_bisecet(c,a)
 
-"""to unpack the txt file"""
-def unpack_words():
-    rev_pair = 0
-    for i in open("words.txt"):
-        if reverse_pair(i) == True:
-            rev_pair = rev_pair + 1
-    return rev_pair                    
 
-print(unpack_words())
-# print(reverse_pair("a"))
+
+reversed_list = reverse_pair_list(open("words.txt"))
+norm_list = normal_list(open("words.txt"))
+for word in reversed_list:
+    if in_bisecet(norm_list, word) == True:
+        print(word[::-1], word)
+
